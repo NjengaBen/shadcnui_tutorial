8ec28fdd5d5032b3b81c1acca04c11bf16c31cc0
NjengaBen <bennjenga2@gmail.com>
2023-11-06 02:06:00 +0300

    home

--- app/page.js
+++ app/page.js
@@ -1,5 +1,38 @@
-import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-export default function Home() {
-  return <main>home</main>;
+async function getRecipes() {
+  const data = await fetch("http://localhost:4000/recipes");
+  return data.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+  return (
+    <main className="grid grid-cols-3 gap-8">
+      {recipes.map((recipe) => {
+        <Card key={recipe.id}>
+          <CardHeader>
+            <div>
+              <CardTitle>{recipe.title}</CardTitle>
+              <CardDescription>{recipe.time} mins to cook. </CardDescription>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <p>{recipe.description}</p>
+          </CardContent>
+          <CardFooter>
+            <button>View Recipe</button>
+            {recipe.vegan && <p>Vegan</p>}
+          </CardFooter>
+        </Card>;
+      })}
+    </main>
+  );
 }
