da9e6a754abae24320d9f5051314afc85caf3f51
NjengaBen <bennjenga2@gmail.com>
2023-11-06 18:23:42 +0300

    images

--- app/page.js
+++ app/page.js
@@ -6,6 +6,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 async function getRecipes() {
   const data = await fetch("http://localhost:4000/recipes");
@@ -16,9 +19,13 @@ export default async function Home() {
   const recipes = await getRecipes();
   return (
     <main className="grid grid-cols-3 gap-8">
-      {recipes.map((recipe) => {
-        <Card key={recipe.id}>
-          <CardHeader>
+      {recipes.map((recipe) => (
+        <Card key={recipe.id} className="flex flex-col justify-between">
+          <CardHeader className="flex-row gap-4 items-center">
+            <Avatar>
+              <AvatarImage src={`/img/${recipe.image}`} alt="recipe image" />
+              <AvatarFallback>{recipe.title.slice(0, 2)}</AvatarFallback>
+            </Avatar>
             <div>
               <CardTitle>{recipe.title}</CardTitle>
               <CardDescription>{recipe.time} mins to cook. </CardDescription>
@@ -27,12 +34,12 @@ export default async function Home() {
           <CardContent>
             <p>{recipe.description}</p>
           </CardContent>
-          <CardFooter>
-            <button>View Recipe</button>
-            {recipe.vegan && <p>Vegan</p>}
+          <CardFooter className="flex justify-between">
+            <Button>View Recipe</Button>
+            {recipe.vegan && <Badge variant="secondary">Vegan</Badge>}
           </CardFooter>
-        </Card>;
-      })}
+        </Card>
+      ))}
     </main>
   );
 }
